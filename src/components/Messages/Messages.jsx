65378304088977bf80ac68c2baa6d614119e1051
euphrit <euphrit1@gmail.com>
2021-03-17 10:09:27 +0300

    textarea for messagePage has been added

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PageTitle from '../PageTitle/PageTitle';
 import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 import style from './Messages.module.css';
@@ -8,22 +9,20 @@ const Messages = (props) => {
 	let dialogsElements =
 		props.state.dialogs.map(d => <Dialog id={d.id} name={d.name} />);
 	let messagesElements =
-		props.state.messages.map(m => <Message message={m.message} />);
+		props.state.messages.map(m => <Message message={m.message} type={m.type} />);
 
 	return (
 		<div>
-			<div className={style.title}>
-				Dialogs
-			</div>
+			<PageTitle title={props.state.title} />
 			<div className={style.flexWrapper}>
 				<div className={style.dialogs}>
 					{dialogsElements}
 				</div>
-
 				<div className={style.messages}>
 					{messagesElements}
 				</div>
 			</div>
+
 		</div >
 	);
 }
